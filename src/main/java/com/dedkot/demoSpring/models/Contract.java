6c94e91da9fed26d117972b04cb34427fa527cee
Dmitry Zhdanov <dmitry.zhdanov01@yandex.ru>
2020-10-31 11:19:30 +0300

    delete field id in contract

--- src/main/java/com/dedkot/demoSpring/models/Contract.java
+++ src/main/java/com/dedkot/demoSpring/models/Contract.java
@@ -1,26 +1,14 @@
 package com.dedkot.demoSpring.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Contract {
     @Id
-    @GeneratedValue
-    private Long id;
-
     private Long idEmployee;
 
     private Long idOrganization;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public Long getIdEmployee() {
         return idEmployee;
