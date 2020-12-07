5d9b41e9ed1e88387e2e71b4f674e8d0436616c0
Dmitry Zhdanov <dmitry.zhdanov01@yandex.ru>
2020-12-07 11:30:16 +0300

    fix delete organization method

--- src/main/java/com/dedkot/demoSpring/repo/EmployeeRepository.java
+++ src/main/java/com/dedkot/demoSpring/repo/EmployeeRepository.java
@@ -3,8 +3,11 @@ package com.dedkot.demoSpring.repo;
 import com.dedkot.demoSpring.models.Employee;
 import com.dedkot.demoSpring.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,5 +22,10 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query("select emp from Employee emp where emp.organization = null")
     List<Employee> findAllFree();
+
+    @Transactional
+    @Modifying
+    @Query("update Employee emp set emp.organization = null where emp.organization = :organization")
+    void dismissAllEmployeesByOrganization(Organization organization);
 
 }
